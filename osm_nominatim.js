b19ef17ae37cfb6162e4e20f69c8e053e0e67089
Gerry Tsui <gerry.tsui@gmail.com>
2011-10-25 01:02:43 +0000

    modified:   osm_nominatim.js

--- osm_nominatim.js
+++ osm_nominatim.js
@@ -1,8 +1,10 @@
-/*Copyright (C) 2011  Gerry Tsui
+/* Address Geocoder with instant candidate and thumbnail map preview.
+Copyright (C) 2011  Gerry Tsui
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation, version 3 of the License
+the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
