4055cf81d57cd7a5e03209bf7b62fea7bc2d5103
Gerry Tsui <gerry.tsui@gmail.com>
2011-10-25 00:57:23 +0000

    modified:   osm_nominatim.js

--- osm_nominatim.js
+++ osm_nominatim.js
@@ -1,3 +1,17 @@
+/*Copyright (C) 2011  Gerry Tsui
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
 var map;
 var poi;
 var osmUSAviewportString = '-128.0,49.0,-65.0,25.0';    
